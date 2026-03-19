a4d953661bb06789d3a7e82c0f3fdca312a8f83d
nassoreen <140000368+nassoreen@users.noreply.github.com>
2026-03-19 23:29:50 +0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from blog.models import QuestionCategory
-from blog.embeddings import embed_text
-
-
-cat = QuestionCategory.objects.create(
-    name="เวลาทำการ",
-    description="คำถามเกี่ยวกับเวลาเปิดปิด",
-    embedding=embed_text("ร้านเปิดกี่โมง ปิดกี่โมง วันหยุด")
-)
